aeb63d34223c8f96e6fd08c8f3c039333ec2ccfa
MaulidyaAyu <111202214310@mhs.dinus.ac.id>
2023-01-13 04:30:56 +0700

    update

--- menu1/contact.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_POST['submit'])) {
-	echo '<h1>Hasil Input</h1>';
-	echo '<ul>';
-	echo '<li>Email: ' . $_POST['email'] . '</li>';
-	echo '<li>Pesan: ' . $_POST['pesan'] . '</li>';
-}?>
